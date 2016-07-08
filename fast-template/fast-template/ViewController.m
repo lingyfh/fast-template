0af6dea774d1b4513ddff6fbe0fcba9eabbf9334
lingyfh <fangshulong@androidesk.com>
2016-07-08 17:19:33 +0800

    init project

---
+++ fast-template/fast-template/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  fast-template
+//
+//  Created by yunfenghan Ling on 7/8/16.
+//  Copyright © 2016 lingyfh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
