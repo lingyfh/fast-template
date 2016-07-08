2e4ac7a7fd6950f146ca98d965a2ef9ba0da2990
lingyfh <fangshulong@androidesk.com>
2016-07-08 17:47:17 +0800

    add pch file pod file

---
+++ fast-template/fast-template/config.h
@@ -0,0 +1,13 @@
+//
+//  config.h
+//  fast-template
+//
+//  Created by yunfenghan Ling on 7/8/16.
+//  Copyright © 2016 lingyfh. All rights reserved.
+//
+
+#import <FFastlib/FFastlib.h>
+
+#pragma mark -
+
+#define appDelegate ((AppDelegate *)[[UIApplication sharedApplication] delegate])
