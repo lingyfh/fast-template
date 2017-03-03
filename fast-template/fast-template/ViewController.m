48658a3a64157a0f1c84bc589aa88b9b88426d2f
lingyfh <LingYFH@gmail.com>
2017-03-03 15:40:58 +0800

    add http request example

--- fast-template/fast-template/ViewController.m
+++ fast-template/fast-template/ViewController.m
@@ -7,15 +7,28 @@
 //
 
 #import "ViewController.h"
+#import "ListViewController.h"
 
 @interface ViewController ()
-
+{
+    ListViewController *listCtrl;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    // 将ringCtril add到viewcontroller上
+    listCtrl = [[self storyboard] instantiateViewControllerWithIdentifier:NSStringFromClass([ListViewController class])];
+    [self addChildViewController:listCtrl];
+    listCtrl.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    [self.view addSubview:listCtrl.view];
+    [listCtrl didMoveToParentViewController:self];
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
